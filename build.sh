c6fc0f130cd3c746b401770285031a1c795f6c3c
scalettar <git@scalettar.com>
2025-04-16 15:48:46 -0700

    rename instances of working-dir to workdir

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ image=$1 &&
 architecture=$2 &&
 
 if [ -z "$architecture" ]; then
-  docker run --rm -v "$(pwd)":/working-dir -v /var/run/docker.sock:/var/run/docker.sock --entrypoint ./bin/create-image sycamore864/ikosu-backend-locations-build:latest "${image}"
+  docker run --rm -v "$(pwd)":/workdir -v /var/run/docker.sock:/var/run/docker.sock --entrypoint ./bin/create-image sycamore864/ikosu-backend-locations-build:latest "${image}"
 else
-  docker run --rm -v "$(pwd)":/working-dir -v /var/run/docker.sock:/var/run/docker.sock --entrypoint ./bin/create-image "sycamore864/ikosu-backend-locations-build:${architecture}" "${image}" "${architecture}"
+  docker run --rm -v "$(pwd)":/workdir -v /var/run/docker.sock:/var/run/docker.sock --entrypoint ./bin/create-image "sycamore864/ikosu-backend-locations-build:${architecture}" "${image}" "${architecture}"
 fi
